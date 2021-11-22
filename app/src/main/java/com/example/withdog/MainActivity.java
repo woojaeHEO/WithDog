8839ed0b040731228914344f9b03db2782540140
PC <kim000183@naver.com>
2021-11-23 00:09:00 +0900

    asdf

--- app/src/main/java/com/example/withdog/MainActivity.java
+++ app/src/main/java/com/example/withdog/MainActivity.java
@@ -8,7 +8,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);//ASDAsda
         setContentView(R.layout.activity_main);
     }
 }
