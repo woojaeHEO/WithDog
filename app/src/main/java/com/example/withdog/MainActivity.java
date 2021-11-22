8f4d33b131b482064f56cd7861fc0197a324cf3f
PC <kim000183@naver.com>
2021-11-23 00:10:41 +0900

    asdfasdfsdafsadfasdfasdfasdfasd

--- app/src/main/java/com/example/withdog/MainActivity.java
+++ app/src/main/java/com/example/withdog/MainActivity.java
@@ -8,7 +8,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);//ASDAsda
+        super.onCreate(savedInstanceState);//ASDAsdaasdfasdfadsf
         setContentView(R.layout.activity_main);
     }
 }
